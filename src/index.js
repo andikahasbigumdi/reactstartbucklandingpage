4a0b7631ac9550abede0c30a74a59e84b4cbdf16
andikahasbigumdi <blog.gumdi@gmail.com>
2021-12-22 14:25:56 +0700

    add working

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Pages from './pages/landing/index.jsx';
 
 ReactDOM.render(
   <>
     <App />
-    <Pages />
   </>,
   document.getElementById('root')
 );
